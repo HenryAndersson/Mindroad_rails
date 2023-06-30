583f1f3f84eb32946a4016e2369ad440c7b7a53b
Henry Andersson <henan555@student.liu.se>
2023-06-30 11:13:39 +0200

    asdas

--- Arduino-stuff/LarmExample/LarmExample.ino
+++ Arduino-stuff/LarmExample/LarmExample.ino
@@ -2,6 +2,8 @@
 #include <Bounce2.h>
 
 Servo servo_pin_2;
+Servo servo_pin_3;
+
 Bounce button1 = Bounce();
 Bounce button2 = Bounce();
 
@@ -15,21 +17,40 @@ void setup() {
 
   servo_pin_2.attach(2, 530, 2600);
   servo_pin_2.write(0);
+
+  servo_pin_3.attach(3, 530, 2600);
+  servo_pin_3.write(0);
   delay(2000);
 
   Serial.println("Klar!");
 }
+
+bool sev1 = false;
+bool sev2 = false;
+
 void loop() {
   button1.update();
   button2.update();
 
   if (button1.fell()) {
-    servo_pin_2.write(90);
-    delay(300);
+    sev1 = !sev1;
+    if (sev1) {
+      servo_pin_2.write(180);
+    } else {
+      servo_pin_2.write(0);
+    }
+
+    delay(100);
   }
 
   if (button2.fell()) {
-    servo_pin_2.write(0);
-    delay(300);
+        sev2 = !sev2;
+    if (sev2) {
+      servo_pin_3.write(180);
+    } else {
+      servo_pin_3.write(0);
+    }
+
+    delay(100);
   }
 }
