ab292696e85dc8a2d4ee3ed647ed8ecb71d6b9a2
Henry Andersson <henan555@student.liu.se>
2023-08-07 04:53:40 +0200

    asdas

--- Arduino-stuff/LarmExample/LarmExample.ino
+++ Arduino-stuff/LarmExample/LarmExample.ino
@@ -1,15 +1,36 @@
-int modulo(int x,int N){
-    return (x % N + N) %N;
-}
+
 
 #include <Servo.h>
 #include <Bounce2.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 
 Servo servo_pin_2;
 Servo servo_pin_3;
 
 Bounce button1 = Bounce();
 Bounce button2 = Bounce();
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 4  // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+int modulo(int x, int N) {
+  return (x % N + N) % N;
+}
+
+void writeString(char* text) {
+  char* c = text;
+  while (*c) {
+    display.write(*c);
+    c++;
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -29,6 +50,23 @@ void setup() {
   servo_pin_3.attach(3, 530, 2600);
   servo_pin_3.write(0);
   delay(2000);
+
+
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(2);       // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);  // Draw white text
+  display.setCursor(0, 0);      // Start at top-left corner
+  display.cp437(true);
+
 
   Serial.println("Klar!");
 }
@@ -67,6 +105,20 @@ void loop() {
     delay(100);
   }
 
-  state = modulo(state,3);
+  state = modulo(state, 3);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  switch (state) {
+    case 0:
+      writeString("LEFT");
+      break;
+    case 1:
+      writeString("MIDDLE");
+      break;
+    case 2:
+      writeString("RIGHT");
+      break;
+  }
+  display.display();
   Serial.println(state);
 }
