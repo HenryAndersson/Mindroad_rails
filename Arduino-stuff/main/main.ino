f4718011e2f84529d3644586a1558051b6916dd2
Henry Andersson <henan555@student.liu.se>
2023-08-09 14:50:23 +0200

    asasdasddsadsaasddas

--- Arduino-stuff/main/main.ino
+++ Arduino-stuff/main/main.ino
@@ -16,14 +16,20 @@ Bounce button2 = Bounce();
 
 #define OLED_RESET 4
 
+#define LED1 7
+#define LED2 8
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 
   pinMode(5, INPUT_PULLUP);
-  button1.attach(5); button1.interval(10);
+  button1.attach(5);
+  button1.interval(10);
 
   pinMode(6, INPUT_PULLUP);
   button2.attach(6);
@@ -50,17 +56,43 @@ void setup() {
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.cp437(true);
-
   Serial.println("Klar!");
+  updateServo(true, 0, 70);
 
-	updateServo(true, 0, 70);
- k
+  digitalWrite(LED1, LOW);
 }
 
 bool sev1 = false;
 bool sev2 = false;
 int state = 1;
 
+unsigned long timer_led1 = 0;
+unsigned long timer_led2 = 0;
+
+unsigned long interval_led = 10;
+
 void loop() {
+  unsigned long currentTime = millis();
+
+  //digitalWrite(LED2, LOW);
+  //delay(100);
+  //digitalWrite(LED1, LOW);
+  //digitalWrite(LED2, HIGH);
+  //delay(100);
+
+#if 1
+  if (digitalRead(6) == HIGH) {
+    digitalWrite(LED1, HIGH);
+  } else {
+    digitalWrite(LED1, LOW);
+  }
+
+  if (digitalRead(5) == HIGH) {
+    digitalWrite(LED2, HIGH);
+  } else {
+    digitalWrite(LED2, LOW);
+  }
+#else
   gate3();
+#endif
 }
